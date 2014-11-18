970d08f9a2d64687d539df77bb34c285f2983d14
inekrest <igor.nekrestyanov@one.verizon.com>
2014-11-18 12:04:41 -0800

    Populate /proxy

--- skydns2.go
+++ skydns2.go
@@ -4,6 +4,7 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+    "fmt"
 
 	"github.com/coreos/go-etcd/etcd"
 )
@@ -11,6 +12,8 @@ import (
 type Skydns2Registry struct {
 	client *etcd.Client
 	path   string
+	domain string
+	scope  string
 }
 
 func NewSkydns2Registry(uri *url.URL) ServiceRegistry {
@@ -19,18 +22,28 @@ func NewSkydns2Registry(uri *url.URL) ServiceRegistry {
 		urls = append(urls, "http://"+uri.Host)
 	}
 
-	return &Skydns2Registry{client: etcd.NewClient(urls), path: domainPath(uri.Path[1:])}
+	parts := strings.SplitN(uri.Path, "/", 2)
+
+	return &Skydns2Registry{client: etcd.NewClient(urls), path: domainPath(parts[3]), domain: parts[3], scope: parts[2]}
 }
 
 func (r *Skydns2Registry) Register(service *Service) error {
 	port := strconv.Itoa(service.Port)
 	record := `{"host":"` + service.IP + `","port":` + port + `}`
-	_, err := r.client.Set(r.servicePath(service), record, uint64(service.TTL))
+	_, err := r.client.Set(r.skydnsPath(service), record, uint64(service.TTL))
+	if (err == nil) {
+		_, err2 := r.client.Set(r.proxyPath(service), record, uint64(service.TTL))
+		return err2
+	}
 	return err
 }
 
 func (r *Skydns2Registry) Deregister(service *Service) error {
-	_, err := r.client.Delete(r.servicePath(service), false)
+	_, err := r.client.Delete(r.skydnsPath(service), false)
+	if (err == nil) {
+		_, err2 := r.client.Delete(r.proxyPath(service), false)
+		return err2
+	}
 	return err
 }
 
@@ -38,14 +51,24 @@ func (r *Skydns2Registry) Refresh(service *Service) error {
 	return r.Register(service)
 }
 
-func (r *Skydns2Registry) servicePath(service *Service) string {
-	return r.path + "/" + service.Name + "/" + strings.Replace(service.ID, ":", "-", -1)
+func (r *Skydns2Registry) skydnsPath(service *Service) string {
+	return r.scope + "/skydns/" + r.path + "/" +
+		   reversePath(service.Name) + "/" + service.pp.ExposedPort + "/" +
+		   strings.Replace(service.ID, ":", "-", -1)
 }
 
-func domainPath(domain string) string {
+func (r *Skydns2Registry) proxyPath(service *Service) string {
+	return fmt.Sprintf("%s/proxy/%s.%s/%s", r.scope, service.pp.ExposedPort, service.Name, r.domain, strings.Replace(service.ID, ":", "-", -1))
+}
+
+func reversePath(domain string) string {
 	components := strings.Split(domain, ".")
 	for i, j := 0, len(components)-1; i < j; i, j = i+1, j-1 {
 		components[i], components[j] = components[j], components[i]
 	}
-	return "/skydns/" + strings.Join(components, "/")
+	return strings.Join(components, "/")
+}
+
+func domainPath(domain string) string {
+	return "/skydns/" + reversePath(domain)
 }
